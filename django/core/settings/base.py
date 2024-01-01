4559a348b414a3afe9788f1c9d884cbbe182e124
Wladimir Pasquel <121689749+lordksix@users.noreply.github.com>
2023-12-31 20:25:10 -0500

    Fix typo

--- django/core/settings/base.py
+++ django/core/settings/base.py
@@ -72,9 +72,9 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("POSTGRES_NAME", "posgres"),
-        "USER": os.environ.get("POSTGRES_USER", "posgres"),
-        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "posgres"),
+        "NAME": os.environ.get("POSTGRES_NAME", "postgres"),
+        "USER": os.environ.get("POSTGRES_USER", "postgres"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "postgres"),
         "HOST": os.environ.get("POSTGRES_HOST", "192.168.0.2"),
         "PORT": 5432,
     }
